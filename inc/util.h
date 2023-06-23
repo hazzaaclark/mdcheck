eb5b6bad2b3f4d2ba7863e43bf7e50ccd914ef8f
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-06-23 21:41:03 +0100

    Update util.h

--- inc/util.h
+++ inc/util.h
@@ -23,13 +23,31 @@
 #if defined(USE_ARGS)
 #define USE_ARGS
 #else
-#define USE_ARGS
+#define USE_HEX_ARGS
 
-#define CHECK_INIT                             0
-#define CHECK_START_OFFSET                 0x200
-#define CHECK_HANDLE_OFFSET                0x18E
-#define CHECK_MASK                         65535
-#define CHECK_BYTES                            2
+#define CHECK_INIT                                0
+#define CHECK_START_OFFSET                    0x200
+#define CHECK_HANDLE_OFFSET                   0x18E
+#define CHECK_MASK                            65535
+#define CHECK_BYTES                               2
+#define BYTE_RANGE                            0x100
+
+#define SUM_LENGTH                                \
+unsigned WORD
+
+typedef struct CHECKSUM
+{
+	typedef U32(*COMPUTE)();
+	typedef FILE* OPEN_FILE;
+	typedef void(*PRINT_SUM)(SUM_LENGTH);
+};
+
+typedef struct FILE_TYPE
+{
+	typedef char CONSOLE_NAME;
+};
+
+#endif
 
 #endif
 
